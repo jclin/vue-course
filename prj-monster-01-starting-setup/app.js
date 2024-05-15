c315b9239324f03f1245c30fb5305c0a72b0be3f
Jerry Lin <jclin18@gmail.com>
2024-05-14 20:02:54 -0700

    Project: Monster Game - implement attack

---
+++ prj-monster-01-starting-setup/app.js
@@ -0,0 +1,34 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + '%' }
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + '%' };
+    }
+  },
+
+  methods: {
+    attackMonster() {
+      this.monsterHealth -= getRandomValue(12, 5);
+      this.attackPlayer();
+    },
+
+    attackPlayer() {
+      this.playerHealth -= getRandomValue(15, 8);
+    }
+  }
+});
+
+app.mount('#game');
