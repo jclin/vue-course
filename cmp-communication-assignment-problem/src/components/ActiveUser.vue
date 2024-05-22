da9e793d6a2c2b255d36d3ee0bfd96d3d9881b5e
Jerry Lin <jclin18@gmail.com>
2024-05-21 18:55:09 -0700

    Assignment 06: Time to Practice Props and Custom Events

---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <h2>Name: {{ username }}</h2>
+    <h3>Age: {{ age }} years young</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent="updateUser">
+    <div>
+      <label>Name: </label>
+      <input type="text" v-model="nameInput" />
+    </div>
+    <div>
+      <label>Age: </label>
+      <input type="number" v-model="ageInput" />
+    </div>
+    <div>
+      <button>Update</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  emits: ["update-user"],
+
+  data() {
+    return {
+      nameInput: this.username,
+      ageInput: this.age,
+    };
+  },
+
+  methods: {
+    updateUser() {
+      this.$emit("update-user", {
+        username: this.nameInput,
+        age: this.ageInput,
+      });
+    },
+  },
+};
+</script>
